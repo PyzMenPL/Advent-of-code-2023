a08bdd709d9f1f3353c954eb194d5fd0e03a8277
PyzMenPL <niskative@proton.me>
2023-12-09 11:03:57 +0100

    Not working prototype of solution for first day :(

---
+++ python/9/part1.py
@@ -0,0 +1,24 @@
+def get_extrapolated_value(numbers: list) -> int:
+    if len(set(numbers)) == 1 and numbers[0] == 0:
+        return numbers[-1]
+    else:
+        diff = []
+
+        for i in range(0, len(numbers)-1):
+            diff.append(abs(numbers[i] - numbers[i+1]))
+
+        return get_extrapolated_value(diff) + numbers[-1] 
+
+
+file = []
+
+with open("input.txt", "r") as F:
+    file = F.readlines()
+
+out = 0
+
+for line in file:
+    numbers = list(map(lambda x: int(x), line.split(" ")))
+    out += get_extrapolated_value(numbers)
+
+print(out)
