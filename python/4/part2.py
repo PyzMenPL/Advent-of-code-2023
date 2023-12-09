8cd1593092d7c6c6562d5c22002d5b486c4570ce
PyzMenPL <niskative@proton.me>
2023-12-09 13:18:05 +0100

    Day 4 solutions in python in english

--- python/4/part2.py
+++ python/4/part2.py
@@ -1,26 +1,34 @@
 def is_number(n: str) -> bool:
     return n.isdigit()
 
-def get_value(n: int) -> int:
-    if n == 0:
-        return 0
-    
-    return 2 ** (n-1)
-
 file = []
 
 with open("input.txt", "r") as F:
     file = [line.strip() for line in F.readlines()]
 
+cards = {}
+
 out = 0
 
-for line in file:
+for n, line in enumerate(file, start=1):
+    if not cards.get(n):
+        cards[n] = 1
+
     line = line.split(": ")[1]
     left, right = line.split(" | ")
     left, right = set(left.split(" ")), set(right.split(" "))
     left, right = set(filter(is_number, left)), set(filter(is_number, right))
     
-    out += get_value(len(left.intersection(right)))
-    
+    new_cards = len(left.intersection(right))
+
+    for _ in range(0, cards[n]):
+        for i in range(n+1, n+1 + new_cards):
+            if cards.get(i):
+                cards[i] += 1 
+            else:
+                cards[i] = 2
+
+    out += cards[n]
+
 print(out)
     
