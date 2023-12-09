db1fb55bcb1a63d0811b6ae52db72f404093800f
PyzMenPL <niskative@proton.me>
2023-12-09 12:41:30 +0100

    Still not working cpp solution

--- c++/6/part2.cpp
+++ c++/6/part2.cpp
@@ -1,9 +1,12 @@
 // 35747 - too low
 // 93 - too low
+// 18446744073709551615 -> unsigned long long
+// 305106211101695
 
 #include <iostream>
 #include <vector>
 #include <cctype>
+#include <unordered_set>
 
 using std::cout;
 using std::endl;
@@ -11,28 +14,43 @@ using std::vector;
 using std::string;
 using std::stoi;
 using std::isdigit;
-
+using std::unordered_set;
 
 int get_dist(int speed, int time_left) {
     return speed * time_left;
 }
 
-void test(unsigned long long czas = 71530, unsigned long long record = 940200) {
-    unsigned long long new_records = 0;
-    unsigned long long last_result = 0;
-    for (int j = 14; j < 71517; j++) {
-        unsigned long long result = get_dist(j, czas-j);
-        
-        if (result < last_result) {
-            new_records *= 2;
-            break;
-        }
-        
-        if (result > record) {
-            new_records++;
-        }
-        
-        last_result = result;
+void test(/*unsigned*/ long long czas = 71530, /*unsigned*/ long long record = 940200) {
+    //unordered_set<long long> known_outputs;
+    long long lowest = 0;
+
+    /*unsigned*/ long long new_records = 1;
+    for (int j = 14; j < czas; j++) {
+        //unsigned long long result = get_dist(j, czas-j);
+       
+	long long before = lowest;//known_outputs.size();
+
+	
+
+	for (int i = 1; i < czas-j; i++) {
+		cout << record - (j * i) << endl;
+		if (record - (j * i) < 0) {
+
+		    cout << record << " - (" << j << " * (" << czas << " - " << j << ")) = " << record - (j * (czas-j)) << endl;
+
+        	    new_records++;
+		    if (record - (j * i) < lowest) {
+			lowest = record - (j * i);
+		    }
+		    //known_outputs.insert(record - (j * i));
+		    break;
+	        }
+	}
+
+	if (before == lowest/*known_outputs.size() && before > 0*/) {
+		new_records *= 2;
+		break;
+	}
     }
    
     cout << new_records-1 << endl;
@@ -41,7 +59,7 @@ void test(unsigned long long czas = 71530, unsigned long long record = 940200) {
 int main()
 {
     test();
-    test(45977295, 305106211101695);
+    //test(45977295, 305106211101695);
 
     return 0;
 }
